4acb58381914e6c099fbd1df065ec0f3f79908b3
Raksha <119318267+raksha2004rk@users.noreply.github.com>
2026-05-03 15:38:17 +0530

    notebook EDA.ipynb

---
+++ src/utils.py
@@ -0,0 +1,27 @@
+import os
+import sys
+import pickle
+
+from src.exception import CustomException
+
+
+# Save object (model, scaler, etc.)
+def save_object(file_path, obj):
+    try:
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
+
+# Load object
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
